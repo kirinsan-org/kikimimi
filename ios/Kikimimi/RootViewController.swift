c38e9376e67eb6a15c816650de6515ace4dbfbba
Jun Tanaka <junpluse@gmail.com>
2016-07-02 20:57:59 +0900

    Initial project setup
    AudioKit + Firebase

---
+++ ios/Kikimimi/RootViewController.swift
@@ -0,0 +1,17 @@
+//
+//  RootViewController.swift
+//  Kikimimi
+//
+//  Created by Jun Tanaka on 7/2/16.
+//  Copyright © 2016 kirinsan.org. All rights reserved.
+//
+
+import UIKit
+
+class RootViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+	}
+
+}
