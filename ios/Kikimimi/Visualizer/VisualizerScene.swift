df15aad79b648714d75c72f6424b8a3bcf699e46
星野恵瑠 <shonetesmith@gmail.com>
2016-07-03 00:51:33 +0900

    Add basic Visualizer classes

---
+++ ios/Kikimimi/Visualizer/VisualizerScene.swift
@@ -0,0 +1,17 @@
+//
+//  VisualizerScene.swift
+//  Kikimimi
+//
+//  Created by 史翔新 on 2016/07/03.
+//  Copyright © 2016年 kirinsan.org. All rights reserved.
+//
+
+import SpriteKit
+
+protocol VisualizerSceneDataSource {
+	func getFFTSampleArray() -> [Double]
+}
+
+class VisualizerScene: SKScene {
+	
+}
---
+++ ios/Kikimimi/Visualizer/VisualizerView.swift
@@ -0,0 +1,21 @@
+//
+//  VisualizerView.swift
+//  Kikimimi
+//
+//  Created by 史翔新 on 2016/07/03.
+//  Copyright © 2016年 kirinsan.org. All rights reserved.
+//
+
+import SpriteKit
+
+class VisualizerView: SKView {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
