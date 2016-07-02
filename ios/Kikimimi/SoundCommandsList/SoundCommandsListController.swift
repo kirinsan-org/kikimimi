de37524597d7f3b1afc4545f84957b17b7d90d4b
星野恵瑠 <shonetesmith@gmail.com>
2016-07-03 04:45:28 +0900

    Add basic SoundCommandList view controller

---
+++ ios/Kikimimi/SoundCommandsList/SoundCommandsListController.swift
@@ -0,0 +1,24 @@
+//
+//  SoundCommandsListController.swift
+//  Kikimimi
+//
+//  Created by 史翔新 on 2016/07/03.
+//  Copyright © 2016年 kirinsan.org. All rights reserved.
+//
+
+import UIKit
+
+class SoundCommandsListController: UITableViewController {
+	
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		
+		// Do any additional setup after loading the view.
+	}
+	
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+	
+}
