8f436195c884b45a0f7cc67d954455ccb1e688d7
Jun Tanaka <junpluse@gmail.com>
2016-07-03 12:01:54 +0900

    Add CircleButton and implement CommandsListController with dummy commands

--- ios/Kikimimi/SoundCommandsList/SoundCommandsListController.swift
+++ ios/Kikimimi/SoundCommandsList/SoundCommandsListController.swift
@@ -8,17 +8,95 @@
 
 import UIKit
 
-class SoundCommandsListController: UITableViewController {
-	
+class SoundCommandsListController: UIViewController {
+
+	private var tableView: UITableView!
+	private var backButton: CircleButton!
+	private var addButton: CircleButton!
+
+	var commands: [Command] = [] {
+		didSet {
+			self.tableView.reloadData()
+		}
+	}
+
 	override func viewDidLoad() {
 		super.viewDidLoad()
-		
-		// Do any additional setup after loading the view.
+
+		tableView = UITableView(frame: view.bounds, style: .Plain)
+		tableView.autoresizingMask = [.FlexibleWidth, .FlexibleHeight]
+		tableView.backgroundColor = UIColor.blackColor()
+		tableView.dataSource = self
+		tableView.delegate = self
+		tableView.rowHeight = 64
+		tableView.separatorInset = UIEdgeInsetsZero
+		tableView.separatorColor = UIColor(white: 0.2, alpha: 1)
+		tableView.registerClass(SoundCommandCell.self, forCellReuseIdentifier: "Command")
+
+		view.addSubview(tableView)
+
+		addButton = CircleButton(imageName: "ic_add_white")
+		addButton.center = CGPoint(x: view.bounds.maxX - (12 + 44 / 2), y: view.bounds.maxY - (12 + 44 / 2))
+		addButton.backgroundColor = UIColor(hue:0.04, saturation:0.88, brightness:0.99, alpha:1)
+
+		backButton = CircleButton(imageName: "ic_arrow_back_white")
+		backButton.center = CGPoint(x: addButton.frame.minX - (8 + 44 / 2), y: addButton.center.y)
+
+		view.addSubview(backButton)
+		view.addSubview(addButton)
+
+//		FirebaseManager.sharedInstance.observerEvent { [weak self] event in
+//			switch event {
+//			case .CommandListChanged(let commands):
+//				self?.commands = commands
+//			default:
+//				break
+//			}
+//		}
+
+		let dummyCommands = [
+			Command(id: "dummy1", name: "とてもおいしいモンテール", action: "", category: CommandCategory(rawValue: 1)!),
+			Command(id: "dummy2", name: "ヨハネちゃんかわいい", action: "", category: CommandCategory(rawValue: 2)!),
+			Command(id: "dummy3", name: "とてもおいしいモンテール", action: "", category: CommandCategory(rawValue: 3)!),
+			Command(id: "dummy4", name: "ヨハネちゃんかわいい", action: "", category: CommandCategory(rawValue: 4)!),
+			Command(id: "dummy5", name: "とてもおいしいモンテール", action: "", category: CommandCategory(rawValue: 5)!),
+			Command(id: "dummy6", name: "ヨハネちゃんかわいい", action: "", category: CommandCategory(rawValue: 6)!),
+			Command(id: "dummy7", name: "とてもおいしいモンテール", action: "", category: CommandCategory(rawValue: 7)!),
+			Command(id: "dummy8", name: "ヨハネちゃんかわいい", action: "", category: CommandCategory(rawValue: 8)!),
+			Command(id: "dummy9", name: "とてもおいしいモンテール", action: "", category: CommandCategory(rawValue: 9)!)
+		]
+		self.commands = dummyCommands
 	}
 	
 	override func didReceiveMemoryWarning() {
 		super.didReceiveMemoryWarning()
-		// Dispose of any resources that can be recreated.
+	}
+
+	override func preferredStatusBarStyle() -> UIStatusBarStyle {
+		return .LightContent
 	}
 	
+}
+
+extension SoundCommandsListController: UITableViewDataSource {
+	func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+		return self.commands.count
+	}
+
+	func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+		let cell = tableView.dequeueReusableCellWithIdentifier("Command") as! SoundCommandCell
+		let command = commands[indexPath.item]
+
+		cell.nameLabel.text = command.name
+		cell.iconView.image = UIImage(named: command.category.imageName)
+		cell.iconView.layer.borderColor = UIColor(hue: CGFloat(command.category.rawValue) / 24, saturation: 0.86, brightness: 1, alpha: 1).CGColor
+
+		return cell
+	}
+}
+
+extension SoundCommandsListController: UITableViewDelegate {
+	func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+
+	}
 }
