7b5a795c561072c0d75dbe4f1c15fa49ed858963
星野恵瑠 <shonetesmith@gmail.com>
2016-07-03 10:38:28 +0900

    Bubble animations update

--- ios/Kikimimi/Visualizer/VisualizerScene.swift
+++ ios/Kikimimi/Visualizer/VisualizerScene.swift
@@ -9,7 +9,7 @@
 import SpriteKit
 
 protocol VisualizerSceneDataSource: class {
-	func getBubbleSettings() -> [UIColor]
+	func getBubbleSettings() -> [(fillColor: SKColor, strokeColor: SKColor)]
 	func getBubbleScales() -> [CGFloat]
 }
 
@@ -18,22 +18,96 @@ class VisualizerScene: SKScene {
 	weak var dataSource: VisualizerSceneDataSource?
 	private let moveActionSpeed: CGFloat = 50
 	
+	private var baseTime: NSTimeInterval = 0
+	private var elapsedFrames: Int = 0
+	
 	override func didMoveToView(view: SKView) {
 		
+		let speed = self.moveActionSpeed
+		
+		self.physicsWorld.gravity = CGVector(dx: 0, dy: 0)
+		
 		let radius: CGFloat = min(self.frame.width, self.frame.height) / 5
+		
+		do {
+			let wall = SKShapeNode(rectOfSize: CGSize(width: self.frame.width, height: 1))
+			wall.strokeColor = .clearColor()
+			wall.fillColor = .clearColor()
+			wall.position = CGPoint(x: self.frame.width / 2, y: 0)
+			
+			let body = SKPhysicsBody(rectangleOfSize: wall.frame.size)
+			body.categoryBitMask = 0x1
+			body.friction = 0
+			body.restitution = 1
+			body.dynamic = false
+			wall.physicsBody = body
+			self.addChild(wall)
+		}
+		
+		do {
+			let wall = SKShapeNode(rectOfSize: CGSize(width: self.frame.width, height: 1))
+			wall.strokeColor = .clearColor()
+			wall.fillColor = .clearColor()
+			wall.position = CGPoint(x: self.frame.width / 2, y: self.frame.height)
+			
+			let body = SKPhysicsBody(rectangleOfSize: wall.frame.size)
+			body.categoryBitMask = 0x1
+			body.friction = 0
+			body.restitution = 1
+			body.dynamic = false
+			wall.physicsBody = body
+			self.addChild(wall)
+		}
+		
+		do {
+			let wall = SKShapeNode(rectOfSize: CGSize(width: 1, height: self.frame.height))
+			wall.strokeColor = .clearColor()
+			wall.fillColor = .clearColor()
+			wall.position = CGPoint(x: 0, y: self.frame.height / 2)
+			
+			let body = SKPhysicsBody(rectangleOfSize: wall.frame.size)
+			body.categoryBitMask = 0x1
+			body.friction = 0
+			body.restitution = 1
+			body.dynamic = false
+			wall.physicsBody = body
+			self.addChild(wall)
+		}
+		
+		do {
+			let wall = SKShapeNode(rectOfSize: CGSize(width: 1, height: self.frame.height))
+			wall.strokeColor = .clearColor()
+			wall.fillColor = .clearColor()
+			wall.position = CGPoint(x: self.frame.width, y: self.frame.height / 2)
+			let body = SKPhysicsBody(rectangleOfSize: wall.frame.size)
+			body.categoryBitMask = 0x1
+			body.friction = 0
+			body.restitution = 1
+			body.dynamic = false
+			wall.physicsBody = body
+			self.addChild(wall)
+		}
 		
 		let bubbleSettings = self.dataSource?.getBubbleSettings() ?? []
 		bubbleSettings.forEach { (color) in
 			let node = BubbleNode(radius: radius)
 			node.lineWidth = 2
-			node.fillColor = SKColor(CGColor: color.CGColor)
-			node.strokeColor = .cyanColor()
+			node.fillColor = color.fillColor
+			node.strokeColor = color.strokeColor
 			node.position = .createRandom(x: radius ... self.size.width - radius, y: radius ... self.size.height - radius)
+			node.setScale(0.5)
 			
-			let speed = self.moveActionSpeed
-			let moveAction = SKAction.moveBy(CGVector(dx: .createRandom(in: -speed ... speed), dy: .createRandom(in: -speed ... speed)), duration: 1)
-			let repeatedAction = SKAction.repeatActionForever(moveAction)
-			node.runAction(repeatedAction)
+			let body = SKPhysicsBody(circleOfRadius: radius)
+			let x = CGFloat.createRandom(in: -speed ... speed)
+			let y = CGFloat.createRandom(in: -speed ... speed)
+			body.velocity = CGVector(dx: x, dy: y)
+			body.categoryBitMask = 0x0
+			body.collisionBitMask = 0x1
+			body.linearDamping = 0
+			body.allowsRotation = false
+			body.friction = 0
+			body.restitution = 1
+			node.physicsBody = body
 			
 			self.addChild(node)
 		}
@@ -42,7 +116,26 @@ class VisualizerScene: SKScene {
 	
 	override func update(currentTime: NSTimeInterval) {
 		
-		let speed = self.moveActionSpeed
+		guard self.baseTime != 0 else {
+			self.baseTime = currentTime
+			return
+		}
+		
+		self.elapsedFrames += 1
+		if self.elapsedFrames < 30 {
+			return
+		}
+		
+		let elapsedTime = currentTime - self.baseTime
+		if elapsedTime > 0.2 {
+			self.baseTime = currentTime
+			self.setScaleForBubbles()
+		}
+		
+	}
+	
+	private func setScaleForBubbles() {
+		
 		let scales = self.dataSource?.getBubbleScales() ?? []
 		
 		self.children.flatMap({ (node) -> BubbleNode? in
@@ -50,51 +143,11 @@ class VisualizerScene: SKScene {
 		}).enumerate().forEach({ (i, bubble) in
 			
 			if i < scales.count {
-				bubble.setScale(scales[i])
+				let action = SKAction.scaleTo(scales[i], duration: 0.2)
+				bubble.runAction(action)
 			} else {
-				bubble.setScale(0.5)
-			}
-			
-			if bubble.frame.top <= self.frame.top && bubble.frame.left <= self.frame.left {
-				let action = SKAction.eternalMove(byX: 0 ... speed, y: 0 ... speed)
-				bubble.removeAllActions()
+				let action = SKAction.scaleTo(0.5, duration: 0.2)
 				bubble.runAction(action)
-				
-			} else if bubble.frame.top <= self.frame.top && bubble.frame.right >= self.frame.right {
-				let action = SKAction.eternalMove(byX: -speed ... 0, y: 0 ... speed)
-				bubble.removeAllActions()
-				bubble.runAction(action)
-				
-			} else if bubble.frame.bottom >= self.frame.bottom && bubble.frame.left <= self.frame.left {
-				let action = SKAction.eternalMove(byX: 0 ... speed, y: -speed ... 0)
-				bubble.removeAllActions()
-				bubble.runAction(action)
-				
-			} else if bubble.frame.bottom >= self.frame.bottom && bubble.frame.right >= self.frame.right {
-				let action = SKAction.eternalMove(byX: -speed ... 0, y: -speed ... 0)
-				bubble.removeAllActions()
-				bubble.runAction(action)
-				
-			} else if bubble.frame.top <= self.frame.top {
-				let action = SKAction.eternalMove(byX: -speed ... speed, y: 0 ... speed)
-				bubble.removeAllActions()
-				bubble.runAction(action)
-				
-			} else if bubble.frame.bottom >= self.frame.bottom {
-				let action = SKAction.eternalMove(byX: -speed ... speed, y: -speed ... 0)
-				bubble.removeAllActions()
-				bubble.runAction(action)
-				
-			} else if bubble.frame.left <= self.frame.left {
-				let action = SKAction.eternalMove(byX: 0 ... speed, y: -speed ... 10)
-				bubble.removeAllActions()
-				bubble.runAction(action)
-				
-			} else if bubble.frame.right >= self.frame.right {
-				let action = SKAction.eternalMove(byX: -speed ... 0, y: -speed ... speed)
-				bubble.removeAllActions()
-				bubble.runAction(action)
-				
 			}
 			
 		})
